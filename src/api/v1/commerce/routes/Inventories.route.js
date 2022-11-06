804464a4bff840ab84021e74b66f886a4bb42369
VolknerSunders <volknereleck@gmail.com>
2022-11-06 13:29:27 -0700

    first commit

---
+++ src/api/v1/commerce/routes/Inventories.route.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'; 
+
+//AQUI EL IMPORTS DE LOS CONTROLADORES.
+
+const router = Router(); 
+
+
+//AQUI VAN LOS LLAMADOS A LOS CONTROLADORES DE LAS APIS
+
+export default router;
---
+++ src/api/v1/commerce/services/Orders.service.js
@@ -0,0 +1,58 @@
+import Orders from '../models/Orders.model';
+import boom from '@hapi/boom';
+ 
+//Eq1: GET LIST Orders
+export const getOrdersList = async () => {
+  let ordersList;
+  try{
+    ordersList = await Orders.find();
+    return(ordersList);
+  }catch(error){
+    throw boom.internal(error);
+  }
+}
+
+//Eq1: GET Orders Item
+export const getOrdersItem = async (id, keyType) => {
+  let ordersItem;
+  try{
+    if(keyType === 'OK'){
+      ordersItem = await Orders.findOne({
+          IdOrdenOK : id
+      });
+    }
+    return(ordersItem);
+  }catch(error){
+    throw boom.internal(error);
+  }
+}
+
+//Eq1: POST (ADD) Orders. 
+export const postOrdersItem = async (paOrdersItem) => { 
+	try { 
+		const newOrdersItem = new Orders(paOrdersItem); 
+		return await newOrdersItem.save(); 
+	} catch (error) { 
+		throw error; 
+	} 
+}
+
+//Eq1: PUT (UPDATE) Orders Item. 
+export const putOrdersItem = async (id, puOrdersItem) => {
+	try {
+		return await Orders.findOneAndUpdate({ IdOrdenOK: id }, puOrdersItem, {
+			new: true,
+		});
+	} catch (error) {
+		throw boom.badImplementation(error);
+	}
+};
+
+//Eq1: DELETE Orders Item. 
+export const deleteOrdersItem = async (id) => {
+	try {
+		return await Orders.findOneAndDelete({ IdOrdenOK: id });
+	} catch (error) {
+		throw boom.badImplementation(error);
+	}
+};
